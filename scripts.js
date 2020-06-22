35129062350d9f9a86be2284186082439377fe58
Marcin Trebacz <emteweb@gmail.com>
2020-06-22 21:27:59 +0200

    tooltip functionality

--- scripts.js
+++ scripts.js
@@ -19,14 +19,56 @@
 
     // beginning Tooltip stuff
 
-    var title = document.querySelectorAll("[title]");
+    var tooltip = null;
 
-    function showTooltip(){
-        console.log("Pokazuję tooltip");
+    // I pass two arguments -> text - for tooltip content and position - for the position :)
+    function createTooltip(text, options){
+
+        var div = document.createElement("div"); // to create a new element in memory
+        
+        div.textContent = text;
+        div.className = "tooltip hidden"; //to add a class to an element (when it has no classes - if it has we can use 'add' method)
+
+        document.body.appendChild(div); // to insert a new element on the website
+
+        div.style.left = (options.x + options.w / 2 - div.offsetWidth / 2) + "px";
+        div.style.top = options.y - div.offsetHeight - 5  + "px"; 
+
+        div.classList.remove("hidden");
+
+        tooltip = div;
+
+
     }
 
-    function hideTooltip(){
-        console.log("Usuwam tooltip");
+    var title = document.querySelectorAll("[title]");
+
+    function showTooltip(e){ 
+
+        var title = e.target.getAttribute("title");
+
+        e.target.removeAttribute("title");
+        // as the second argument I pass an object (key-value pairs)
+        createTooltip(title, {
+            x: e.target.offsetLeft,
+            y: e.target.offsetTop,
+            w: e.target.offsetWidth
+        });
+    }
+/*
+    -- 'e' is a parameter to which the browser will return an object with 'MouseEvent' class
+       to see this object => console.log(e);
+    -- We can refer to different properties of the object using . notation 
+       console.log(e.target) => shows the element on which the function called
+    -- we can get the attribute of the chosen element:
+       console.log(e.target.getAttribute("title"));
+    */
+
+    function hideTooltip(e){
+
+        e.target.setAttribute("title", tooltip.textContent);
+        tooltip.parentNode.removeChild(tooltip); // to remove element from page
+        
     }
 
     for (var i = 0; i < title.length; i++){
